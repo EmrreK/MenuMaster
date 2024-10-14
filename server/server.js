c9aede7189c683eeaa6dc55512a681cf405fa037
Emre Kadri <emrexanthi@gmail.com>
2024-10-14 16:23:39 +0300

    initial project setup

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const session = require("express-session");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet");
+require("dotenv").config();
+const {PORT, MONGO_URI, SESSION_SECRET} = process.env;
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.use(
+	session({secret: SESSION_SECRET, resave: false, saveUninitialized: false})
+);
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
