6f68cc7cc0d6f55190e4004837645d15ef2e2daa
Emre Kadri <emrexanthi@gmail.com>
2024-10-21 00:27:50 +0300

    Added react-scroll for scroll animation

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import {Link} from "react-scroll";
 
 function Navbar() {
 	return (
-		<div data-theme="dark" className="text-white fixed w-full z-10">
+		<div data-theme="dark" className="text-white fixed w-full z-10 ">
 			<div className="navbar p-3 ">
 				<div className="navbar-start">
 					<a href=" " className="btn  text-2xl">
@@ -14,14 +15,19 @@ function Navbar() {
 						<li className="mr-6 ">
 							<a href=" ">Home</a>
 						</li>
-						<li className="mr-6 ">
+						<li className="">
+							<Link to="features" smooth={true} duration={500}>
+								Features
+							</Link>
+						</li>
+						<li className="">
 							<a href=" ">Demo</a>
 						</li>
 						<li className="mr-6 ">
-							<a href=" ">Contact</a>
-						</li>
-						<li className="">
 							<a href=" ">Pricing</a>
+						</li>
+						<li className="mr-6 ">
+							<a href=" ">Contact</a>
 						</li>
 					</ul>
 				</div>
