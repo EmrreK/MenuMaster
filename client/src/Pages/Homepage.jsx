e1cb8195387731bc3e8d7bf22d644282096f5611
Emre Kadri <emrexanthi@gmail.com>
2024-10-20 19:00:00 +0300

    Features added

--- client/src/Pages/Homepage.jsx
+++ client/src/Pages/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Hero from "../Components/Hero";
+import Features from "../Components/Features";
 
 function Homepage() {
 	return (
@@ -8,6 +9,7 @@ function Homepage() {
 			<div>
 				<Navbar />
 				<Hero />
+				<Features />
 			</div>
 		</div>
 	);
