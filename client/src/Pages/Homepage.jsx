5749a8beb5211b6ca90a6dafe06e37bcc540b022
Eymen Imam <eymenimam5@gmail.com>
2024-10-21 17:14:54 +0300

    BigFeatures added

--- client/src/Pages/Homepage.jsx
+++ client/src/Pages/Homepage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Hero from "../Components/Hero";
 import Features from "../Components/Features";
+import BigFeature from "../Components/BigFeature";
 
 function Homepage() {
 	return (
@@ -10,6 +11,7 @@ function Homepage() {
 				<Navbar />
 				<Hero />
 				<Features />
+				<BigFeature />
 			</div>
 		</div>
 	);
